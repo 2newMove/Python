286af828553541d14060c00776701d9cd5668e8e
2newMove <118728010+2newMove@users.noreply.github.com>
2022-11-26 14:21:29 +0500

    can't get the answer right
    if you know what i am doing be sure to tell me too

---
+++ problem 3.py
@@ -0,0 +1,37 @@
+# link : https://www.hackerrank.com
+# PreparePython > Introduction > Loops
+# Task
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+# Example
+# The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+# 0
+# 1
+# 4
+# Input Format
+# The first and only line contains the integer, .
+# Constraints
+# Output Format
+# Print  lines, one corresponding to each .
+# Sample Input 0
+# 5
+# Sample Output 0
+# 0
+# 1
+# 4
+# 9
+# 16
+
+############## My code 
+n = int(input())
+
+if n >= 0 and n <= 20:
+    While n == 0:
+        print(n**n)
+        n= n-1 
+
+############## Issue 
+# not getting the results except errors.
+# line 28 :  While n == 0:
+
+
+
